0d3152defa6bedf94205e33900c7721c5236c209
Jeff Hanna <jeff.b.hanna@gmail.com>
2020-12-04 15:52:25 -0600

    formatting update

--- jeff_hanna/4.py
+++ jeff_hanna/4.py
@@ -23,32 +23,11 @@ def validate_passport_keys( raw_data ):
 
 
 def validate_passport_values( passports ):
-    """
-    Validate based on these rules
-    byr (Birth Year) - four digits; at least 1920 and at most 2002.
-    iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-    eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-    hgt (Height) - a number followed by either cm or in:
-    If cm, the number must be at least 150 and at most 193.
-    If in, the number must be at least 59 and at most 76.
-    hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-    ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-    pid (Passport ID) - a nine-digit number, including leading zeroes.
-    cid (Country ID) - ignored, missing or not.
-
-    Args:
-        valid_passports ( [ ]dict ):
-
-    Returns:
-        [ ]dict:
-    """
-
     def _int_validator( val, min, max ):
         if val.isdigit( ) and min <= int( val ) <= max:
             return True
         
         return False
-
 
     valid_passport_count = 0
 

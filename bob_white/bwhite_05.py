3c927693a4ef970e9e6c43ae09f591469d4b1f42
bob <robert.wd.white@gmail.com>
2020-12-04 23:30:55 -0600

    Some cleanup on day 05.

--- bob_white/bwhite_05.py
+++ bob_white/bwhite_05.py
@@ -1,21 +1,20 @@
 pzl = open("day_05.input").read().splitlines()
 passes = []
-for test in pzl:
+for boarding_pass in pzl:
     rows = range(128)
     columns = range(8)
-    for val in test:
+    for val in boarding_pass:
         if val == "F":
             rows = rows[: len(rows) // 2]
         elif val == "B":
             rows = rows[len(rows) // 2 :]
-        if val == "R":
+        elif val == "R":
             columns = columns[len(columns) // 2 :]
         elif val == "L":
             columns = columns[: len(columns) // 2]
-    passes.append(next(iter(rows)) * 8 + next(iter(columns)))
+    passes.append(rows[0] * 8 + columns[0])
 
 print(f"Part_01 {max(passes)}")
 
-missing = set(passes).symmetric_difference(range(min(passes), max(passes)))
-missing = {v for v in missing if v not in {min(passes), max(passes)}}
+missing = set(range(min(passes), max(passes))).difference(passes)
 print(f"Part_02 {missing.pop()}")

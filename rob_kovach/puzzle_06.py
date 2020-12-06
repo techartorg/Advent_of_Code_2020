eae444fa3cb6b545b5eb5e5b25a901aa343105e8
rgkovach123 <rgkovach123@gmail.com>
2020-12-05 23:22:49 -0600

    Day 06 Solution

---
+++ rob_kovach/puzzle_06.py
@@ -0,0 +1,50 @@
+test_input = '''abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b
+'''
+
+location = __file__
+groups = open(location.replace('.py', '_input.txt')).read().split('\n\n')
+
+def get_answers(group):
+    answers = 0
+    for i in 'abcdefghijklmnopqrstuvwxyz':
+        if i in group:
+            answers += 1
+    return answers
+
+def get_actual_answers(group):
+    answers = 0
+    individuals = group.splitlines()
+    members = len(individuals)
+    for i in 'abcdefghijklmnopqrstuvwxyz':
+        count = 0
+        for member in individuals:
+            if i in member:
+                count += 1
+        if count == members:
+            answers += 1
+    return answers
+
+
+def part1():
+    return sum(get_answers(x) for x in groups)
+
+def part2():
+    return sum(get_actual_answers(x) for x in groups)
+
+if __name__ == '__main__':
+    print(part1())
+    print(part2())

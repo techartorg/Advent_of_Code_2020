70f1852fc618b50b0b00477492435e4855d14efe
bob <robert.wd.white@gmail.com>
2020-12-05 23:19:46 -0600

    I like using set.union a bit more in part 1 Makes it more like part 2.

--- bob_white/bwhite_06.py
+++ bob_white/bwhite_06.py
@@ -2,6 +2,5 @@ puzzle = open("day_06.input").read().split("\n\n")
 
 from functools import reduce
 
-print(sum(len(set(c for c in group if c not in "\n")) for group in puzzle))
-
+print(sum(len(reduce(set.union, (set(person) for person in group.split("\n")))) for group in puzzle))
 print(sum(len(reduce(set.intersection, (set(person) for person in group.split("\n")))) for group in puzzle))

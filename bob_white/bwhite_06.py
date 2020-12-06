67adef48049477d7bcf171b9a50e3a5bda951260
bob <robert.wd.white@gmail.com>
2020-12-05 23:10:48 -0600

    Better names

--- bob_white/bwhite_06.py
+++ bob_white/bwhite_06.py
@@ -2,6 +2,6 @@ puzzle = open("day_06.input").read().split("\n\n")
 
 from functools import reduce
 
-print(sum(len(set(c for c in item if c not in "\n")) for item in puzzle))
+print(sum(len(set(c for c in group if c not in "\n")) for group in puzzle))
 
-print(sum(len(reduce(set.intersection, (set(line) for line in item.split("\n")))) for item in puzzle))
+print(sum(len(reduce(set.intersection, (set(person) for person in group.split("\n")))) for group in puzzle))

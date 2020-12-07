40bc96e51126f729ae5a8cfa2e273a20d8bd5ad7
bob <robert.wd.white@gmail.com>
2020-12-07 00:26:41 -0600

    Adding some comments so I don't forget

--- bob_white/bwhite_07.py
+++ bob_white/bwhite_07.py
@@ -11,15 +11,18 @@ for rule in rules:
             bag_map[outer].append((int(contains[0]), " ".join(contains[1:-1])))
 
 
+# This initial contains check here is needed, because otherwise the bag_size changes during iteration because its a defaultdict
 def contains_shiny_gold(bag):
     return bag in bag_map and any(color == "shiny gold" or contains_shiny_gold(color) for _count, color in bag_map[bag])
 
 
 print(sum(contains_shiny_gold(bag) for bag in bag_map))
 
-
+# need to add 1 to account for the actual bag itself.
 def count_inner_bags(bag):
     return 1 + sum((count_inner_bags(inner_bag) * count) for count, inner_bag in bag_map[bag])
 
 
+# Need to subtract 1 because we only care about bags inside our shiny gold bag, and we've accounted for it in the function.
+# Stupid off by 1 issues will be he death of me.
 print(count_inner_bags("shiny gold") - 1)

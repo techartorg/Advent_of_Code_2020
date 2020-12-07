c2a47314e841e1b913e9f3394f1d6fc5fb9ea2dd
bob <robert.wd.white@gmail.com>
2020-12-06 22:37:57 -0600

    Because I can't stop fiddling with some of these.

--- bob_white/bwhite_05.py
+++ bob_white/bwhite_05.py
@@ -18,8 +18,10 @@ for boarding_pass in pzl:
 # New hotness!
 # So the insight here is that because we're actually multiplying the row by 8 before adding,
 # we're actually just creating a 10-bit number
-
 passes = sorted(int("".join("0" if c in "FL" else "1" for c in boarding_pass), 2) for boarding_pass in pzl)
+
+# So this is the same as above, but just adds the powers of 2 together. Reversing the boarding_pass string is because I can't get enumate to count down.
+passes = sorted(sum(2 ** idx for idx, c in enumerate(boarding_pass[::-1]) if c in "BR") for boarding_pass in pzl)
 print(f"Part_01 {passes[-1]}")
 
 missing = set(range(passes[0], passes[-1])).difference(passes)

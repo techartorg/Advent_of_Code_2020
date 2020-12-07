c09e4344cbde4b701eea13a9141433f0e38a8c26
bob <robert.wd.white@gmail.com>
2020-12-07 00:21:37 -0600

    Happier with this version
    Moved the count / color parsing out of part 2 and into the initial map building.
    
    This let me simplify both part 1 and 2 _a lot_.

--- bob_white/bwhite_07.py
+++ bob_white/bwhite_07.py
@@ -1,35 +1,25 @@
+from collections import defaultdict
+from typing import List, Tuple, Dict
+
 rules = open("day_07.input").read().splitlines()
-
-bag_map = {}
+bag_map: Dict[str, List[Tuple[int, str]]] = defaultdict(list)
 for rule in rules:
-    outer, *_, inner = rule.partition(" bags contain ")
-    bag_map[outer] = inner
+    outer, *_, contents = rule.partition(" bags contain ")
+    if "no other" not in contents:
+        for c in contents.split(", "):
+            contains = c.split()
+            bag_map[outer].append((int(contains[0]), " ".join(contains[1:-1])))
 
 
-valid_bags = {"shiny gold"}
-all_bags = list(bag_map)
-valid_count = len(valid_bags)
+def contains_shiny_gold(bag):
+    return bag in bag_map and any(color == "shiny gold" or contains_shiny_gold(color) for _count, color in bag_map[bag])
 
-while True:
-    for bag in all_bags:
-        if any(b in bag_map[bag] for b in valid_bags):
-            valid_bags.add(bag)
-    all_bags = [bag for bag in all_bags if bag not in valid_bags]
-    if len(valid_bags) == valid_count:
-        print(len(valid_bags) - 1)
-        break
-    valid_count = len(valid_bags)
+
+print(sum(contains_shiny_gold(bag) for bag in bag_map))
 
 
 def count_inner_bags(bag):
-    if "no other" in bag_map[bag]:
-        return 1
-    c = 1
-    for contains in bag_map[bag].split(", "):
-        count, *name, _ = contains.split()
-        name = " ".join(name)
-        c += int(count) * count_inner_bags(name)
-    return c
+    return 1 + sum((count_inner_bags(inner_bag) * count) for count, inner_bag in bag_map[bag])
 
 
 print(count_inner_bags("shiny gold") - 1)

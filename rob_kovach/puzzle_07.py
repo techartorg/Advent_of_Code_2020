f05babc98a129d1775dfc694036a5781810cf3fa
rgkovach123 <rgkovach123@gmail.com>
2020-12-07 09:03:57 -0600

    Day 7 Part 2 solution and part 1 refactor

--- rob_kovach/puzzle_07.py
+++ rob_kovach/puzzle_07.py
@@ -2,36 +2,44 @@
 Advent of Code - Day 7
 """
 
-""" Test Input
-baggage = '''light red bags contain 1 bright white bag, 2 muted yellow bags.
-dark orange bags contain 3 bright white bags, 4 muted yellow bags.
-bright white bags contain 1 shiny gold bag.
-muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
-shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
-dark olive bags contain 3 faded blue bags, 4 dotted black bags.
-vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
-faded blue bags contain no other bags.
-dotted black bags contain no other bags.
-'''
-"""
-
 location = __file__
 baggage = open(location.replace('.py', '_input.txt')).read().splitlines()
 
+# Dictionary of Bags where the string name is the key and the value is a Bag Object.
 bags = {}
 
+class Bag():
+    def __init__(self, color):
+        self.color = color
+        # maintain a dictionary of the child bags and their count.
+        self.children = {}
+        self.parents = []
+
 def collect_bags():
-    """
-    Pre-Populate the dictionary of bags with the color.
+    """Pre-Populate the dictionary of bags with the color and their children.
+    
+    Mostly just string parsing...
+    
     """
     for bag in baggage:
         color = bag.split(' contain')[0].split('bags')[0].strip()
-        c = bag.split(' contain ')[-1].split(',')
-        bags[color] = []
+        b = Bag(color)
+        bags[color] = b
+
+        children = bag.split(' contain ')[-1].split(',')
+        for x in children:
+            if 'no other' in x:
+                continue
+            child = x.split('bag')[0].strip()
+            count = int(child[0])
+            color2 = child[2:]
+            b.children[color2] = count
 
 def populate_bags():
-    """
-    Do a second pass on all the bag colors, recording who their parent is.
+    """Do a second pass on all the bags, recording their parent.
+    
+    More string parsing...
+    
     """
     for bag in baggage:
         color = bag.split(' contain')[0].split('bags')[0].strip()
@@ -41,23 +49,42 @@ def populate_bags():
                 continue
             b = x.split('bag')[0].strip()
             color2 = b[2:]
-            bags[color2].append(color)
+            bags[color2].parents.append(color)
 
 def get_parents(bag):
+    """Recurse through parents of the specified bag.
+
+    The fun part!
+
     """
-    Recurse through parents of the specified bag.
-    """
-    parents = bags[bag]
+    parents = bags[bag].parents
     for parent in parents:
         grandparents = get_parents(parent)
         parents = list(set(parents + grandparents))
     return parents
+
+def count_bags(bag):
+    """
+    Recurse through the contained bags to determine the total 
+    number of bags within.
+    
+    """
+    count = 0
+    for child, num in bags[bag].children.items():
+        count += num
+        count += count_bags(child) * num
+    return count
 
 def part1():
     collect_bags()
     populate_bags()
     return len(get_parents('shiny gold'))
 
+def part2():
+    collect_bags()
+    populate_bags()
+    return count_bags('shiny gold')
+
 if __name__ == '__main__':
     print(part1())
-
+    print(part2())

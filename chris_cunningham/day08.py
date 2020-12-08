a17c2a235f4cf75ed6a756ef394c856e0b9ceb7e
Chris Cunningham <chris@rightsomegoodgames.ca>
2020-12-08 13:16:45 -0400

    some cleanup

--- chris_cunningham/day08.py
+++ chris_cunningham/day08.py
@@ -1,10 +1,10 @@
 from typing import Dict, Callable, Tuple, List
+import inspect
 
 
 def machine(ins: List[Tuple[str, int]]):
     pointer = 0
     acc = 0
-    visited = set()
 
     def op_nop(_: int):
         nonlocal pointer
@@ -24,6 +24,8 @@ def machine(ins: List[Tuple[str, int]]):
         "acc": op_acc,
         "jmp": op_jmp,
     }
+
+    visited = set()
 
     while pointer < len(ins):
         if pointer in visited:
@@ -52,8 +54,8 @@ with open("inputs/day08.txt", 'r') as f:
             new_instructions = instructions[:]
             new_instructions[i] = (new_op, arg)
 
-            acc = 0
             try:
+                acc = 0
                 for a in machine(new_instructions):
                     acc = a
                 print(f"part b: {acc}")

91640f80eb448b186b54baa0b0b39cdee2a10b95
Jeff Hanna <jeff.b.hanna@gmail.com>
2020-12-10 20:03:54 -0600

    10.2 was awful.

--- jeff_hanna/10.py
+++ jeff_hanna/10.py
@@ -3,6 +3,9 @@
 from contextlib import suppress
 from pathlib import Path
 
+_MIN_JOLTAGE = 1
+_MAX_JOLTAGE = 3
+
 def _part_1( adaptors ):
     num_1_volt_diffs = 0
     num_3_volt_diffs = 0
@@ -10,12 +13,24 @@ def _part_1( adaptors ):
     for i, joltage in enumerate( adaptors ):
         with suppress( IndexError ):
             diff = adaptors[ i + 1 ] - joltage
-            if diff == 1:
+            if diff == _MIN_JOLTAGE:
                 num_1_volt_diffs += 1
-            elif diff == 3:
+            elif diff == _MAX_JOLTAGE:
                 num_3_volt_diffs += 1
             
     return num_1_volt_diffs * num_3_volt_diffs
+
+
+def _part_2( adaptors ):
+    tree_sums = [ 1 ] + [ 0 ] * ( len( adaptors ) - 1 ) # each element contains the joltage sum of a possible chain of adaptors.
+    for i, joltage in enumerate( adaptors ):
+        j = i + 1
+        while j < len( adaptors ) and adaptors[ j ] - joltage <= _MAX_JOLTAGE:
+            tree_sums[ j ] += tree_sums[ i ]
+            j += 1
+        
+    return tree_sums[ -1 ]
+
 
 
 if __name__ == '__main__':
@@ -23,7 +38,8 @@ if __name__ == '__main__':
     
     # Account for the wall socket and the device.
     adaptors.insert( 0, 0 )
-    adaptors.append( adaptors[ -1 ] + 3 )
+    adaptors.append( adaptors[ -1 ] + _MAX_JOLTAGE )
     
     # Part 1
-    print( _part_1( adaptors ) ) # higher than 1827
+    print( _part_1( adaptors ) )
+    print( _part_2( adaptors[ : -1 ] ) ) 

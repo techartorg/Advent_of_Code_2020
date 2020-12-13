0191dbb8390371275f9e6fc35d2ee07780de3c49
bob <robert.wd.white@gmail.com>
2020-12-13 00:32:57 -0600

    Day 13

---
+++ bob_white/bwhite_13.py
@@ -0,0 +1,29 @@
+from operator import mul
+from collections import deque
+from math import gcd
+
+time, buses = open("day_13.input").read().splitlines()
+
+bus_ids = [(idx, int(bus)) for idx, bus in enumerate(buses.split(",")) if bus != "x"]
+closest = []
+for _, bus in bus_ids:
+    closest.append(((int(time) // bus) * bus - int(time) + bus, bus))
+print(mul(*min(closest)))
+
+
+bus_deque = deque(bus_ids)
+t, step = bus_deque.popleft()
+while bus_deque:
+    idx, bus_id = bus_deque[0]
+    # Keep steping foward by the next multiple of our busses.
+    if (t + idx) % bus_id:
+        t += step
+    else:
+        # When the remainder is zero, we can add a new bus to the system
+        # so we need to get a new LCM - least common multiple from our old
+        # time step, and the new bus_id
+        # Then we can pop off a new bus and work our way through the list.
+        step = abs(step * bus_id) // gcd(step, bus_id)
+        bus_deque.popleft()
+
+print(t)

3356886b54dba111765e56ca36a6ad2869e0706d
bob <robert.wd.white@gmail.com>
2020-12-14 00:34:26 -0600

    Day 14, slightly clearner Though relying on setitem side effects is not clean. At least not in real code.

--- bob_white/bwhite_14.py
+++ bob_white/bwhite_14.py
@@ -1,5 +1,6 @@
 import re
 from collections import defaultdict
+from operator import setitem
 
 pzl = open("day_14.input").read().splitlines()
 
@@ -30,9 +31,9 @@ for line in pzl:
         # But we start with the unmasked address, then update each bit based on the mask, creating 2 new address for any X (with 0 and 1 values)
         for idx, v in enumerate(mask):
             if v == "1":
-                addrs[:] = [[av if i != idx else "1" for i, av in enumerate(addr)] for addr in addrs]
+                addrs[:] = [addr[:] for addr in addrs if not setitem(addr, idx, "1")]
             elif v == "X":
-                addrs[:] = [[av if i != idx else nv for i, av in enumerate(addr)] for addr in addrs for nv in ("0", "1")]
+                addrs[:] = [addr[:] for addr in addrs for nv in ("0", "1") if not setitem(addr, idx, nv)]
         for addr in addrs:
             memory[int("".join(addr), 2)] = int(val)
 print(sum(memory.values()))

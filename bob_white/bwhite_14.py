9f8ba5b45d241d53b0acb8f81ca641b0e9293c03
bob <robert.wd.white@gmail.com>
2020-12-14 11:31:01 -0600

    Day 14 - Much happier with how this looks now. Wrote a specialized replace function instead of relying on setitem side effects. This also removed the slice-copy from the list comprehension. Changed from enumerating the mask, to generating a mask-mapping lookup for getting the replacement values. Means I can skip over `0`'s in the mask earlier and removes some branches from the loop.

--- bob_white/bwhite_14.py
+++ bob_white/bwhite_14.py
@@ -1,6 +1,12 @@
 import re
 from collections import defaultdict
-from operator import setitem
+
+
+def replace_at_index(collection, index, value):
+    col = collection[:]
+    col[index] = value
+    return col
+
 
 pzl = open("day_14.input").read().splitlines()
 
@@ -13,31 +19,24 @@ for line in pzl:
         mask = val
     elif loc.startswith("mem"):
         int_addr = int(regex.findall(loc)[0])
-        bin_val = "".join(a if b == "X" else b for a, b in zip(bin(int(val))[2:].rjust(36, "0"), mask))
+        bin_val = "".join(a if b == "X" else b for a, b in zip(f"{int(val):036b}", mask))
         memory[int_addr] = int(bin_val, 2)
 print(sum(memory.values()))
 
 memory.clear()
-mask = ""
+mask_map = {}
 for line in pzl:
     loc, val = line.split(" = ")
     if loc == "mask":
-        mask = val
+        mask_map = {idx: ["1"] if v == "1" else ["0", "1"] for idx, v in enumerate(val) if v != "0"}
     elif loc.startswith("mem"):
         int_addr = int(regex.findall(loc)[0])
-        bin_addr = bin(int_addr)[2:].rjust(36, "0")
-        addrs = [list(bin_addr)]
+        bin_addr = f"{int(int_addr):036b}"
         # Creating the collection of addresses from the mask is a little funny.
         # But we start with the unmasked address, then update each bit based on the mask, creating 2 new address for any X (with 0 and 1 values)
-        for idx, v in enumerate(mask):
-            if v == "1":
-                new_vals = ["1"]
-            elif v == "X":
-                new_vals = ["0", "1"]
-            else:
-                continue
-            # lot of slicing, mostly we need to ensure that we're saving off a copy each iteration, so that we don't mutate the original
-            addrs[:] = [addr[:] for addr in addrs for nv in new_vals if not setitem(addr, idx, nv)]
+        addrs = [list(bin_addr)]
+        for idx, new_vals in mask_map.items():
+            addrs[:] = [replace_at_index(addr, idx, nv) for addr in addrs for nv in new_vals]
         for addr in addrs:
             memory[int("".join(addr), 2)] = int(val)
 print(sum(memory.values()))

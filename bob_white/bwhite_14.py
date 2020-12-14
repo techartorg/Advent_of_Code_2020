3cac5172119ef01f100db6008f97181984876513
bob <robert.wd.white@gmail.com>
2020-12-14 14:25:33 -0600

    Way to easy to keep noodling on this.

--- bob_white/bwhite_14.py
+++ bob_white/bwhite_14.py
@@ -21,18 +21,18 @@ for line in pzl:
         mask = val
         mask_map = {idx: ["1"] if v == "1" else ["0", "1"] for idx, v in enumerate(val) if v != "0"}
     elif loc.startswith("mem"):
-        int_addr = int(regex.findall(loc)[0])
-        bin_addr = f"{int(int_addr):036b}"
-        bin_val = "".join(a if b == "X" else b for a, b in zip(f"{int(val):036b}", mask))
-        part_01[bin_addr] = int(bin_val, 2)
+        bin_address = f"{int(regex.findall(loc)[0]):036b}"
+        masked_val = "".join(a if b == "X" else b for a, b in zip(f"{int(val):036b}", mask))
+        part_01[bin_address] = int(masked_val, 2)
 
         # Creating the collection of addresses from the mask is a little funny.
         # But we start with the unmasked address, then update each bit based on the mask, creating 2 new address for any X (with 0 and 1 values)
-        addrs = [list(bin_addr)]
+        masked_addresses = [list(bin_address)]
         for idx, new_vals in mask_map.items():
-            addrs[:] = [replace_at_index(addr, idx, nv) for addr in addrs for nv in new_vals]
-        for addr in addrs:
-            part_02["".join(addr)] = int(val)
+            masked_addresses[:] = [replace_at_index(address, idx, nv) for address in masked_addresses for nv in new_vals]
+        assert len(masked_addresses) == 2 ** mask.count("X")  # We amke a lot of masked_addresses.
+        for address in masked_addresses:
+            part_02["".join(address)] = int(val)
 
 print(f"Part_01: {sum(part_01.values())}")
 print(f"Part_02: {sum(part_02.values())}")

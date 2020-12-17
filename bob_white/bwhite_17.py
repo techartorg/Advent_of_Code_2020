f4c59d85a6d743a667fa689eec5e071945e9750a
bob <robert.wd.white@gmail.com>
2020-12-17 13:29:11 -0600

    Faster method for 17 This no longer relies on expanding the search space each step. Instead I capture all the points surrounding active nodes. Then search those nodes + the active nodes. Cuts my search space down by about a third for part 2.

--- bob_white/bwhite_17.py
+++ bob_white/bwhite_17.py
@@ -3,33 +3,30 @@ from collections import defaultdict
 
 pzl = open("day_17.input").read().splitlines()
 
-directions = [v for v in product((-1, 0, 1), repeat=4) if any(v)]
-
 # Part 1
 cube_01 = defaultdict(int)
-cube_01.update({(x, y, 0, 0): int(v == "#") for y, line in enumerate(pzl) for x, v in enumerate(line)})
+cube_01.update({(x, y, 0): int(v == "#") for y, line in enumerate(pzl) for x, v in enumerate(line)})
+directions_01 = [v for v in product((-1, 0, 1), repeat=3) if any(v)]
 
-for step in range(1, 7):
-    mins = [0 - step, 0 - step, 0 - step, 0]
-    maxs = [8 + step, 8 + step, 1 + step, 1]
+for _ in range(0, 6):
     new_cube = defaultdict(int)
-    for loc in product(*(range(min_, max_) for min_, max_ in zip(mins, maxs))):
-        active = sum(cube_01[tuple(sum(v) for v in zip(loc, d))] for d in directions)
+    for loc in {tuple(sum(v) for v in zip(loc, d)) for loc in cube_01 for d in directions_01 if cube_01[loc]}:
+        active = sum(cube_01[tuple(sum(v) for v in zip(loc, d))] for d in directions_01)
         new_cube[loc] = cube_01[loc] and active in (2, 3) or not cube_01[loc] and active == 3
     cube_01 = new_cube.copy()
 print(sum(cube_01.values()))
 
-
 # Part 2
 cube_02 = defaultdict(int)
 cube_02.update({(x, y, 0, 0): int(v == "#") for y, line in enumerate(pzl) for x, v in enumerate(line)})
+directions_02 = [v for v in product((-1, 0, 1), repeat=4) if any(v)]
 
-for step in range(1, 7):
-    mins = [0 - step, 0 - step, 0 - step, 0 - step]
-    maxs = [8 + step, 8 + step, 1 + step, 1 + step]
+cube_02 = defaultdict(int)
+cube_02.update({(x, y, 0, 0): int(v == "#") for y, line in enumerate(pzl) for x, v in enumerate(line)})
+for _ in range(0, 6):
     new_cube = defaultdict(int)
-    for loc in product(*(range(min_, max_) for min_, max_ in zip(mins, maxs))):
-        active = sum(cube_02[tuple(sum(v) for v in zip(loc, d))] for d in directions)
+    for loc in {tuple(sum(v) for v in zip(loc, d)) for loc in cube_02 for d in directions_02 if cube_02[loc]}:
+        active = sum(cube_02[tuple(sum(v) for v in zip(loc, d))] for d in directions_02)
         new_cube[loc] = cube_02[loc] and active in (2, 3) or not cube_02[loc] and active == 3
     cube_02 = new_cube.copy()
 print(sum(cube_02.values()))

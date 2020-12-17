53e154a6dbcd5a4ef873f7aef88178bd6e7dad57
bob <robert.wd.white@gmail.com>
2020-12-17 01:39:34 -0600

    Some name cleanup.

--- bob_white/bwhite_17.py
+++ bob_white/bwhite_17.py
@@ -1,47 +1,41 @@
 from itertools import product
 from collections import defaultdict
-from typing import Dict, Tuple
 
 pzl = open("day_17.input").read().splitlines()
 
 directions = [v for v in product((-1, 0, 1), repeat=4) if any(v)]
 
 # Part 1
-cube: Dict[Tuple[int, int, int, int], int] = defaultdict(int)
-for y, line in enumerate(pzl):
-    for x, v in enumerate(line):
-        cube[(x, y, 0, 0)] = int(v == "#")
+cube_01 = defaultdict(int)
+cube_01.update({(x, y, 0, 0): int(v == "#") for y, line in enumerate(pzl) for x, v in enumerate(line)})
 
 for step in range(1, 7):
     mins = [0 - step, 0 - step, 0 - step, 0]
     maxs = [8 + step, 8 + step, 1 + step, 1]
-    new_cube: Dict[Tuple[int, int, int, int], int] = defaultdict(int)
-    loc: Tuple[int, int, int, int]
-    for loc in product(*(range(a, b) for a, b in zip(mins, maxs))):
-        active = sum(cube[tuple(sum(v) for v in zip(loc, d))] for d in directions)
-        if cube[loc] and active in (2, 3):
+    new_cube = defaultdict(int)
+    for loc in product(*(range(min_, max_) for min_, max_ in zip(mins, maxs))):
+        active = sum(cube_01[tuple(sum(v) for v in zip(loc, d))] for d in directions)
+        if cube_01[loc] and active in (2, 3):
             new_cube[loc] = 1
-        elif not cube[loc] and active == 3:
+        elif not cube_01[loc] and active == 3:
             new_cube[loc] = 1
-    cube = new_cube.copy()
-print(sum(cube.values()))
+    cube_01 = new_cube.copy()
+print(sum(cube_01.values()))
 
 
 # Part 2
-cube = defaultdict(int)
-for y, line in enumerate(pzl):
-    for x, v in enumerate(line):
-        cube[(x, y, 0, 0)] = int(v == "#")
+cube_02 = defaultdict(int)
+cube_02.update({(x, y, 0, 0): int(v == "#") for y, line in enumerate(pzl) for x, v in enumerate(line)})
 
 for step in range(1, 7):
     mins = [0 - step, 0 - step, 0 - step, 0 - step]
     maxs = [8 + step, 8 + step, 1 + step, 1 + step]
     new_cube = defaultdict(int)
-    for loc in product(*(range(a, b) for a, b in zip(mins, maxs))):
-        active = sum(cube[tuple(sum(v) for v in zip(loc, d))] for d in directions)
-        if cube[loc] and active in (2, 3):
+    for loc in product(*(range(min_, max_) for min_, max_ in zip(mins, maxs))):
+        active = sum(cube_02[tuple(sum(v) for v in zip(loc, d))] for d in directions)
+        if cube_02[loc] and active in (2, 3):
             new_cube[loc] = 1
-        elif not cube[loc] and active == 3:
+        elif not cube_02[loc] and active == 3:
             new_cube[loc] = 1
-    cube = new_cube.copy()
-print(sum(cube.values()))
+    cube_02 = new_cube.copy()
+print(sum(cube_02.values()))

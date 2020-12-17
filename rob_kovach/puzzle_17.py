75c21c6eb7814d963bd0527a0bd223de4fb43ddb
rgkovach123 <rgkovach123@gmail.com>
2020-12-17 17:12:39 -0600

    Day 17, part 1 code cleanup

--- rob_kovach/puzzle_17.py
+++ rob_kovach/puzzle_17.py
@@ -2,7 +2,7 @@
 Advent of Code - Day 17
 """
 
-from copy import deepcopy
+from copy import deepcopy, copy
 from collections import defaultdict
 
 
@@ -16,24 +16,20 @@ input_ = open(location.replace('.py', '_input.txt')).read()
 
 
 # Part 1 ----------------------------------------------------------------------
-"""
-Super messy...
-
-"""
-
 # Store the position and state of each cube as a dictionary.
 # Unknown positions default to Inactive.
-CUBE_STATES = defaultdict(bool)
-
-starting_condition = [list(line) for line in input_.splitlines()]
-
-for y, i in enumerate(starting_condition):
+INITIAL_STATE = defaultdict(bool)
+for y, i in enumerate([list(line) for line in input_.splitlines()]):
     for x, j in enumerate(i):
-        CUBE_STATES[(x, y, 0)] = j == '#'
+        INITIAL_STATE[(x, y, 0)] = j == '#'
 
-# add empty cells around the starting data
+
 def grow_space(cycle):
-    new_cycle = deepcopy(cycle)
+    """
+    Adds empty cells around the data to prepare it for the next power cycle.
+    
+    """
+    new_cycle = copy(cycle)
     for cube in cycle.keys():
         x, y, z = cube
         for x1 in range(-1, 2):
@@ -43,25 +39,27 @@ def grow_space(cycle):
                         new_cycle[(x+x1, y+y1, z+z1)] = False
     return new_cycle
 
-def is_cube_active(x, y, z, cycle):
-    return cycle[(x, y, z)]
+def is_cube_active(position, cycle):
+    return cycle[position]
 
-def get_active_neighbor_count(x, y, z, cycle):
+def get_active_neighbor_count(position, cycle):
     activeNeighbors = 0
     for x1 in range(-1, 2):
         for y1 in range(-1, 2):
             for z1 in range(-1, 2):
+                # Get off the offset position from our cube.
+                newPosition = tuple(map(sum, zip(position, (x1, y1, z1))))
                 # Don't count ourselves.
-                if (x, y, z) == (x+x1, y+y1, z+z1):
+                if position == newPosition:
                     continue
-                if is_cube_active(x+x1, y+y1, z+z1, cycle):
+                if is_cube_active(newPosition, cycle):
                     activeNeighbors += 1
     return activeNeighbors, cycle
 
-def get_state(x, y, z, cycle):
-    activeNeighbors, updatedCycle = get_active_neighbor_count(x, y, z, cycle)
+def get_state(position, cycle):
+    activeNeighbors, updatedCycle = get_active_neighbor_count(position, cycle)
     state = False
-    if is_cube_active(x, y, z, updatedCycle):
+    if is_cube_active(position, updatedCycle):
         if activeNeighbors in [2, 3]:
             state = True
     else:
@@ -70,25 +68,22 @@ def get_state(x, y, z, cycle):
     return state, updatedCycle
 
 def boot_process():
-
-    previous_round = grow_space(deepcopy(CUBE_STATES))
-    current_round = deepcopy(previous_round)
+    previous_round = grow_space(INITIAL_STATE)
+    current_round = copy(previous_round)
 
     max_cycles = 6
     for i in range(max_cycles):
         cubes = list(previous_round.keys())
-        updatedCycle = deepcopy(previous_round)
         for cube in cubes:
-            x, y, z = cube
-            active, updatedCycle = get_state(x, y, z, updatedCycle)
-            current_round[(x, y, z)] = active
+            active, previous_round = get_state(cube, previous_round)
+            current_round[cube] = active
         
-        previous_round = grow_space(deepcopy(current_round))
+        previous_round = grow_space(current_round)
 
     return sum(state for state in current_round.values())
 
 
-print(boot_process())
+print(f'Part 1: {boot_process()}')
 
 # Part 2 ----------------------------------------------------------------------
 """
@@ -163,4 +158,4 @@ def boot_process():
     return sum(state for state in current_round.values())
 
 
-print(boot_process())
+#print(boot_process())

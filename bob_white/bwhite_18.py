317306ba795534673b4a877e70d39bee8eed6468
bob <robert.wd.white@gmail.com>
2020-12-18 01:13:43 -0600

    Names.

--- bob_white/bwhite_18.py
+++ bob_white/bwhite_18.py
@@ -1,27 +1,27 @@
 import re
 
 vals = re.compile(r"\d+? [*+] \d+")
-num = re.compile(r"\(\d+?\)")
+nums = re.compile(r"\(\d+?\)")
 pzl = open("day_18.input").read().splitlines()
 part_01 = []
 for line in pzl:
-    while (f := vals.findall(line)) :
-        line = vals.sub(str(eval(f[0])), line, 1)
-        while (v := num.findall(line)) :
-            line = num.sub(str(eval(v[0])), line, 1)
+    while (val := vals.findall(line)) :
+        line = vals.sub(str(eval(val[0])), line, 1)
+        while (num := nums.findall(line)) :
+            line = nums.sub(str(eval(num[0])), line, 1)
     part_01.append(int(line))
 print(sum(part_01))
 
 
 adds = re.compile(r"\d+? \+ \d+")
-group = re.compile(r"\(([^)(]+)\)")
+groups = re.compile(r"\(([^)(]+)\)")
 
 
 def search(expr):
-    while grp := group.findall(expr):
-        expr = group.sub(search(grp[0]), expr, 1)
-    while ad := adds.findall(expr):
-        expr = adds.sub(str(eval(ad[0])), expr, 1)
+    while group := groups.findall(expr):
+        expr = groups.sub(search(group[0]), expr, 1)
+    while add := adds.findall(expr):
+        expr = adds.sub(str(eval(add[0])), expr, 1)
 
     return str(eval(expr))
 

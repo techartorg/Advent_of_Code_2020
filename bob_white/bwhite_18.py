1c2e76cba8b1f5c94a19d754741100c89bca7ddd
bob <robert.wd.white@gmail.com>
2020-12-18 08:16:28 -0600

    Day 18, object based. Way less parsing, but still can't get away from regex.

--- bob_white/bwhite_18.py
+++ bob_white/bwhite_18.py
@@ -27,3 +27,30 @@ def search(expr):
 
 
 print(sum(int(search(line)) for line in pzl))
+
+# Messing with order of operations is silly and evil. But totally works.
+# so basic idea for part 1, is get both __add__ and __mul__ on the same precident level.
+# which means moving multiplcation to __sub__
+class Part1(int):
+    def __add__(self, b):
+        return type(self)(self.real + b)
+
+    def __sub__(self, b):
+        return type(self)(self.real * b)
+
+
+# Part 2 we basically want to swap the precident level
+# which is pretty easy to do by swapping the operators
+class Part2(int):
+    def __mul__(self, b):
+        return type(self)(self.real + b)
+
+    def __add__(self, b):
+        return type(self)(self.real * b)
+
+
+digits = re.compile(r"(\d+)")
+# same as the Part1 class above, swapping mul for sub
+print(sum(eval(digits.sub(r"Part1(\1)", line.replace("*", "-"))) for line in pzl))
+# Slighly longer song and dance to swap add and mul, but like the Part2 class, that is what we need to do here.
+print(sum(eval(digits.sub(r"Part2(\1)", line.replace("*", "-").replace("+", "*").replace("-", "+"))) for line in pzl))

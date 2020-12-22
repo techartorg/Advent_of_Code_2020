53f5a08330963f63b269175c26dbfaa8ab217724
bob <robert.wd.white@gmail.com>
2020-12-22 00:18:50 -0600

    Forgot about islice, much nice way to slice a deque. Also moving the state checks inside the function. Possibly faster with it all being locals now? Seems faster.

--- bob_white/bwhite_22.py
+++ bob_white/bwhite_22.py
@@ -1,4 +1,5 @@
-from collections import defaultdict, deque
+from collections import deque
+from itertools import islice
 
 
 player_01, player_02 = open("day_22.input").read().split("\n\n")
@@ -17,18 +18,16 @@ while p1_cards and p2_cards:
 print(sum(i * card for i, card in enumerate(reversed(p1_cards if p1_cards else p2_cards), 1)))
 
 
-existing_rounds = defaultdict(set)
-
-
 def recursive_combat(p1_cards, p2_cards, game=1):
 
     winner = 1
+    existing_rounds = set()
     while p1_cards and p2_cards:
         # sets don't like deques, so we'll make tuples. So many tuples.
         current_hand = tuple(map(tuple, (p1_cards, p2_cards)))
-        if current_hand in existing_rounds[game]:
+        if current_hand in existing_rounds:
             return 1
-        existing_rounds[game].add(current_hand)
+        existing_rounds.add(current_hand)
 
         p1 = p1_cards.popleft()
         p2 = p2_cards.popleft()
@@ -38,13 +37,10 @@ def recursive_combat(p1_cards, p2_cards, game=1):
             else:
                 winner = 2
         else:
-            # Can't slice a deque, so we have to list and slice,
-            # though I guess we could copy, and pop from the bottom
-            rp1_cards = deque(list(p1_cards)[:p1])
-            rp2_cards = deque(list(p2_cards)[:p2])
-            # This should in theory get us a unique game-id for each call.
-            # otherwise spawning multiple sub-games from the same game-name would reuse the id
-            winner = recursive_combat(rp1_cards, rp2_cards, max(existing_rounds) + 1)
+            # Slicing a deque is complicated!
+            rp1_cards = deque(islice(p1_cards, 0, p1))
+            rp2_cards = deque(islice(p2_cards, 0, p2))
+            winner = recursive_combat(rp1_cards, rp2_cards, game + 1)
 
         if winner == 1:
             p1_cards.extend([p1, p2])
